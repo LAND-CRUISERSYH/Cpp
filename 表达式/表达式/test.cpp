67a40b5a1798aa817e5c96652dba8b9bf7d755ca
LAND-CRUISERSYH <1354843647@qq.com>
2020-09-02 23:44:28 +0800

    表达式

---
+++ 表达式/表达式/test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main()
+{
+	//cout << 5 + 10 * 20 / 2 << endl;
+
+	/*vector<int> ivec = { 0,2,4,6,8 };
+	cout << *ivec.begin() << endl;
+	cout << *ivec.begin() + 1 << endl;
+	cout << *(ivec.begin()) << endl;
+	cout << (*ivec.begin()) + 1 << endl;*/
+}
