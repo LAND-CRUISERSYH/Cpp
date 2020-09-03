c4088c0a94467842c4d7c46dd24732f7bf153c44
LAND-CRUISERSYH <1354843647@qq.com>
2020-09-03 18:07:15 +0800

    优先级结合律

--- 表达式/表达式/test.cpp
+++ 表达式/表达式/test.cpp
@@ -15,4 +15,24 @@ int main()
 	cout << *ivec.begin() + 1 << endl;
 	cout << *(ivec.begin()) << endl;
 	cout << (*ivec.begin()) + 1 << endl;*/
+
+	/*int i = 0;
+	cout << i << " " << ++i << endl;*/
+
+	/*short value = 32767;
+	value += 1;
+	cout << value << endl;*/
+
+	/*int a = 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2;
+	cout << a << endl;*/
+
+	//cout << -30 * 3 + 21 / 5 << endl;                //-86
+	//cout << -30 + 3 * 21 / 5 << endl;               //-18
+	//cout << 30 / 3 * 21 % 5 << endl;                 //0
+	//cout << -30 / 3 * 21 % 4 << endl;                //-2
+
+	/*vector<int> ivec = { 1,2,3 };
+	cout << ivec.empty() << endl;*/
+
+	
 }
