a6ab95a52753bf67d36f8819b0269071b1815b66
LAND-CRUISERSYH <1354843647@qq.com>
2020-09-03 22:53:59 +0800

    运算符

--- 表达式/表达式/test.cpp
+++ 表达式/表达式/test.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::string;
 
 int main()
 {
@@ -34,5 +36,48 @@ int main()
 	/*vector<int> ivec = { 1,2,3 };
 	cout << ivec.empty() << endl;*/
 
+	/*string s = "hello";
+	string s2 = "hello" + s;*/
+
+	//vector<string> svec = { "hello","world" };
+	//vector<string>::iterator iter = svec.begin();
+	//cout << *iter++ << endl;            //hello
+	////cout << (*iter)++ << endl;                //错误
+	////cout << *iter.empty() << endl;                //没有成员empty();
+	//cout << iter->empty() << endl;             //0
+	////cout << ++*iter << endl;                        //错误
+	//cout << iter++->empty() << endl;
+
+	///*使用条件运算符从vector<int>中找到奇数并翻倍*/
+	//vector<int> ivec = { 1,5,9,3,4,5,6,2,7,8,6,4,70,42,66 };
+	//for (auto i = ivec.begin(); i != ivec.end(); i++)
+	//{
+	//	if ((*i % 2 == 0) ? 1 : 0)
+	//	{
+	//		*i *= 2;
+	//		cout << *i << " ";
+	//	}
+	//}
 	
+	/*string s = "word";
+	cout << s.size();*/
+
+	///*输出每一种内置类型所占空间的大小*/
+	//cout << sizeof(int) << endl;                      //4
+	//cout << sizeof(char) << endl;                   //1
+	//cout << sizeof(float) << endl;                   //4
+	//cout << sizeof(double) << endl;                //8
+	//cout << sizeof(int *) << endl;                         //4
+	//cout << sizeof(short) << endl;                   //2
+	//cout << sizeof(bool) << endl;                    //1
+	//cout << sizeof(long) << endl;                   //4
+	//cout << sizeof(long long) << endl;                //8
+
+	//int x[10];
+	//int *p = x;
+	//cout << sizeof(x) / sizeof(*x) << endl;               //10
+	//cout << sizeof(p) / sizeof(*p) << endl;               //1
+
+	vector<int> ivec = { 1,2,3,4,5,6,7,8,9 };
+	vector<int>::size_type cnt = ivec.size()
 }
