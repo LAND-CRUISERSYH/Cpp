e45c701a231131e5f104d1ad71a843aeb32b8dbe
LAND-CRUISERSYH <1354843647@qq.com>
2020-09-12 23:26:13 +0800

    定义抽象数据类型

--- 类/类/test.cpp
+++ 类/类/test.cpp
@@ -5,6 +5,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::istream;
+using std::ostream;
 
 struct Sales_data
 {
@@ -14,7 +16,12 @@ struct Sales_data
 	double revenue;
 	Sales_data &combine(const Sales_data &rhs);
 	string isbn()const;
+	double avg_price()const;
 };
+
+Sales_data add(const Sales_data &book1, const Sales_data &book2);
+istream &read(istream &is, Sales_data &item);
+ostream &print(ostream &os, const Sales_data &item);
 
 struct Person
 {
@@ -28,7 +35,11 @@ struct Person
 	{
 		return address;
 	}
+	Person(string s, string add) :name(s), address(add) {}
 };
+
+istream &read_Person(istream &is, Person &p);
+ostream &print_Person(ostream &os, const Person &p);
 
 Sales_data::Sales_data()
 {
@@ -41,6 +52,41 @@ Sales_data& Sales_data::combine(const Sales_data &rhs)
 	sales_vol += rhs.sales_vol;
 	revenue += rhs.revenue;
 	return *this;
+}
+
+double Sales_data::avg_price() const
+{
+	return revenue / sales_vol;
+}
+
+Sales_data add(const Sales_data &book1, const Sales_data &book2)
+{
+	Sales_data sum = book1;
+	sum.combine(book2);
+	return sum;
+}
+istream &read(istream &is, Sales_data &item)
+{
+	double price = 0;
+	is >> item.name >> item.sales_vol >> price;
+	item.revenue = item.sales_vol*price;
+	return is;
+}
+ostream &print(ostream &os, const Sales_data &item)
+{
+	os << item.name << " " << item.sales_vol << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+istream &read_Person(istream &is, Person &p)
+{
+	is >> p.name >> p.address;
+	return is;
+}
+ostream &print_Person(ostream &os, const Person &p)
+{
+	os << p.name << " " << p.address;
+	return os;
 }
 
 int main(int argc, char **argv)
@@ -70,7 +116,7 @@ int main(int argc, char **argv)
 		return -1;
 	}*/
 
-	Sales_data total;
+	/*Sales_data total;
 	if (cin >> total.name >> total.sales_vol >> total.revenue)
 	{
 		Sales_data trans;
@@ -87,6 +133,28 @@ int main(int argc, char **argv)
 			}
 		}
 		cout << total.name << " " << total.sales_vol << " " << total.revenue << endl;
+	}
+	else
+	{
+		std::cerr << "没有数据" << endl;
+		return -1;
+	}*/
+
+	Sales_data total;
+	if (read(cin, total))
+	{
+		Sales_data trans;
+		while (read(cin, trans))
+		{
+			if (total.name == trans.name)
+				total.combine(trans);
+			else
+			{
+				print(cout, total);
+				total = trans;
+			}
+		}
+		print(cout, total);
 	}
 	else
 	{
